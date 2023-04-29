20897533c465ca4fd4134cfee84d1cf2ef2bf43e
Gustavo Cantador <grgcantador@gmail.com>
2023-04-28 22:09:53 -0300

    dotenv

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from decouple import config
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-DATABASE_URI = config('DATABASE_URI')
+DATABASE_URI = os.getenv('DATABASE_URI')
 
 app = Flask(__name__) # creates a flask app with the name of the file as a parameter
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' # configure database path
