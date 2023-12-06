909085c5c0de78dfb367771d43595f5e784ed888
USER <grgcantador@gmail.com>
2023-12-06 13:26:24 -0300

    render upload

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -6,18 +6,11 @@ from flask_jwt_extended import JWTManager
 from datetime import timedelta
 from flask_cors import CORS
 import os
-#load_dotenv()
 
-#DATABASE_URI = os.getenv('DATABASE_URI')
-
-app = Flask(__name__) # creates a flask app with the name of the file as a parameter
+app = Flask(__name__) #  creates a flask app with the name of the file as a parameter
 CORS(app)
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI")
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://doe_vida_database_user:2xkoGzBu5fDNye2PLfwZff9Msq6fEK0K@dpg-cjrr5kojbais73fd2mf0-a.oregon-postgres.render.com/doe_vida_database' OLD
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://doe_vida_database2_user:18ilykINyAoUevWguk4OoqQPjI3IyG0Z@dpg-clnp65ofvntc73b5iueg-a.oregon-postgres.render.com/doe_vida_database2'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///seu_banco_de_dados.db' # local data base for development
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI")
 app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
-app.config["JWT_SECRET_KEY"] = 'zGEHihvhaCxwaOnLfsPB6isU96YQzbV4'
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 jwt__ = JWTManager(app)
